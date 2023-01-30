bf5d6ac7c6f816b0ab4f0a69a25bf81d3bca68df
Artiqk <dexkyo98@gmail.com>
2023-01-30 16:51:36 +0100

    Added cache + used find instead of findAll()

--- cve_web_scraper.py
+++ cve_web_scraper.py
@@ -1,14 +1,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import requests_cache
 from cvss_trans import *
 
 base_url = "https://nvd.nist.gov/vuln/detail/cve-"
 
+requests_cache.install_cache('html_cache')
+
 def retrieve_html_information(url, beacon, attr, attr_value):
 	html_page = urlopen(url) # On ouvre la page html et on la stock dans la variable
 	html_parse = BeautifulSoup(html_page, 'html.parser') # Instancie un object BeautifulSoup qui permet de parser du HTML grâce à html.parser
-	result =  html_parse.findAll(beacon, {attr: attr_value}) # Récupère les lignes HTML qui possèdes ces attribues
-	return result[0].get_text() # Retourne le resultat sans les balises
+	result =  html_parse.find(beacon, {attr: attr_value}) # Récupère les lignes HTML qui possèdes ces attribues
+	return result.get_text() # Retourne le resultat sans les balises
 
 
 def get_cve_info(cve_number): # TODO - Add threads to retrieve informations
@@ -20,8 +23,11 @@ def get_cve_info(cve_number): # TODO - Add threads to retrieve informations
     return published_on, vector, description, cve_score, # FIXME - Put this in a dictionary
 
 
-# published_on, vector, description, cve_score = get_cve_info("2016-5195")
-# print(cve_score)
+published_on, vector, description, cve_score = get_cve_info("2016-5195")
+print(published_on)
+print(vector)
+print(description)
+print(cve_score)
 
 # ####### T'occupes pas #######
 
