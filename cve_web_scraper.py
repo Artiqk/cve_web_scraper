1327ede3d7d7fc9db9b135e7dde01fc81d1335d8
Artiqk <dexkyo98@gmail.com>
2023-01-30 13:07:30 +0100

    Added WIP function

--- cve_web_scraper.py
+++ cve_web_scraper.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from cvss_trans import *
 
 base_url = "https://nvd.nist.gov/vuln/detail/cve-"
 
@@ -10,10 +11,23 @@ def retrieve_html_information(url, beacon, attr, attr_value):
 	return result[0].get_text() # Retourne le resultat sans les balises
 
 
-def get_cve_info(cve_number): # FIXME - Add threads to retrieve informations
+def get_cve_info(cve_number): # TODO - Add threads to retrieve informations
     url = base_url + cve_number
     published_on = retrieve_html_information(url, "span", "data-testid", "vuln-published-on")
     vector = retrieve_html_information(url, "span", "data-testid", "vuln-cvss3-nist-vector")
-    return published_on, vector
+    description = retrieve_html_information(url, "p", "data-testid", "vuln-description")
+    cve_score = retrieve_html_information(url, "a", "data-testid", "vuln-cvss3-panel-score")
+    return published_on, vector, description, cve_score, # FIXME - Put this in a dictionary
 
-print(get_cve_info("2016-5195"))
+
+# published_on, vector, description, cve_score = get_cve_info("2016-5195")
+# print(cve_score)
+
+# ####### T'occupes pas #######
+
+# def get_detailed_vector(vector):
+#     vectors = vector.split('/')
+#     vectors.pop(0)
+#     for vector in vectors:
+        
+# detailed_vector = get_detailed_vector(vector)
