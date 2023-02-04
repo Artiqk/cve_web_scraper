fd59c6e75decc4f2d4f4b279a2c59bc2d408fc52
Artiqk <dexkyo98@gmail.com>
2023-02-04 18:27:16 +0100

    WIP - filter + excel

--- cve_web_scraper.py
+++ cve_web_scraper.py
@@ -1,25 +1,30 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests_cache
+import requests
 from cvss_trans import *
 
-base_url = "https://nvd.nist.gov/vuln/detail/cve-"
+base_url = "https://nvd.nist.gov/vuln/detail/"
 
 requests_cache.install_cache('html_cache')
 
-def retrieve_html_information(url, beacon, attr, attr_value):
+def retrieve_html_information(url, beacon, attr='', attr_value='', find_all=False):
     try:
         html_page   = urlopen(url)
         html_parse  = BeautifulSoup(html_page, 'lxml')
-        result      = html_parse.select_one(f"{beacon}[{attr}='{attr_value}']")
-        return result.get_text() 
+        if find_all:
+            result = html_parse.findAll(beacon, {attr: attr_value})
+            return result
+        else:
+            result = html_parse.select_one(f"{beacon}[{attr}='{attr_value}']") # p[data-testid='description']
+            return result.get_text() 
     except: # FIXME - Add better error handling
         print("An error occured while trying to retrieve information on the page")
         return ""
 
 
-def get_cve_info(cve_number): # TODO - Add threads to retrieve informations
-    url = base_url + cve_number
+def get_cve_info(cve_id): # TODO - Add threads to retrieve informations
+    url = base_url + cve_id
     published_on = retrieve_html_information(url, "span", "data-testid", "vuln-published-on")
     vector = retrieve_html_information(url, "span", "data-testid", "vuln-cvss3-nist-vector")
     description = retrieve_html_information(url, "p", "data-testid", "vuln-description")
@@ -43,8 +48,26 @@ def convert_to_detailed_cvss(cvss):
     return detailed_cvss
 
 
-cve_info = get_cve_info("2016-5195")
+def get_cve_list_from_filter(url):
+    response = requests.get(redhat_url)
+    html_parse = BeautifulSoup(response.content, 'lxml')
+    return [cve.get_text().lower() for cve in html_parse.findAll('b')]
 
-for value in cve_info.values():
-    if "CVSS" in value:
-        print(convert_to_detailed_cvss(value))
+
+def export_cve_info(cve):
+    cve_info = get_cve_info(cve)
+    for value in cve_info.values():
+        if "CVSS" in value:
+            print(convert_to_detailed_cvss(value))
+        else:
+            print(value)
+    print("======================================================================================")
+
+
+
+redhat_url = "https://access.redhat.com/hydra/rest/securitydata/cve?after=2022-01-01&before=2023-01-01&severity=important&per_page=2"
+
+cves = get_cve_list_from_filter(redhat_url)
+
+for cve in cves:
+    export_cve_info(cve)
